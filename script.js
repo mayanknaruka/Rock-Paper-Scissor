9a2e88a0d00f6e89ee2fd80d840f8c6d72fa2284
mayanknaruka <mayanknaruka47@gmail.com>
2026-01-11 20:24:34 +0530

    Step 5: Write the logic to play a single round

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Step 2: Write the logic to get the computer choice
 
-const { get } = require("mongoose")
-
 function getComputerChoice() {
     let randomNumber = Math.floor((Math.random() * 3))
 
@@ -16,7 +14,6 @@ function getComputerChoice() {
     }
 }
   
-
 //    Step 3: Write the logic to get the human choice
 
 function getHumanChoice() {
@@ -26,13 +23,52 @@ function getHumanChoice() {
 // console.log(getHumanChoice());
 
 // step 6:
+ 
+      
 // Step 4: Declare the players score variables
 
 let humanScore = 0
 let computerScore = 0
 
+// Step 5: Write the logic to play a single round
+   
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
 
 
+  if (humanChoice==="rock" && computerChoice==="scissor") {
+    console.log("You win, rock beats scissors");
+    humanScore++
+  }
+  else if (humanChoice==="paper"&& computerChoice==="rock"){
+        console.log("You win, paper beats rock");
+        humanScore++
+  }
+  
+  else if (humanChoice==="scissor"&& computerChoice==="paper"){
+        console.log("You win, scissor beats paper");
+        humanScore++
+  }
+
+else if (humanChoice  === computerChoice){
+        console.log(`It's a tie! Both choose ${humanChoice}`);
+        humanScore++
+  }
+  
+  else if (humanChoice==="scissor"&& computerChoice==="rock"){
+        console.log("You loose, scissor beats rock");
+        computerScore++
+  }
 
 
+    else if (humanChoice==="rock"&& computerChoice==="paper"){
+        console.log("You loose, rock beats paper");
+        computerScore++
+  }
 
+    else if (humanChoice==="paper"&& computerChoice==="scissor"){
+        console.log("You loose, paper beats scissor");
+        computerScore++
+  }
+
+}
